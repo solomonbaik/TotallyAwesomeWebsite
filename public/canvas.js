fd2099e9efa8e970d330a53ebd07df6010f1f848
zachansell <89551318+zachansell@users.noreply.github.com>
2022-01-18 14:15:30 -0800

    it draws, but the frame-delayed translation doesnt work

--- public/canvas.js
+++ public/canvas.js
@@ -1,26 +1,30 @@
-function drawPic()
+function drawPic(ctx)
 {
   ctx.save();
-  var time2 = new Date();
-  ctx.translate(time2.getSeconds(), 0);
+  //var time2 = new Date();
+  //ctx.translate(time2.getSeconds(), 0);
+  //drawing oval
 
   ctx.beginPath();
   ctx.moveTo(200,200);
-  ctx.quadraticCurveTo(-40,50,200,300);
-  ctx.quadraticCurveTo(40,50,200,200);
+  ctx.quadraticCurveTo(160,250,200,300);
+  ctx.quadraticCurveTo(240,250,200,200);
+  frameNum++;
+  frameNum=frameNum%30;
+  var translation = 0;
+  if (frameNum<16)
+  {
+    translation=75
+  }
+  ctx.translate(translation, translation);
   ctx.stroke();
+
   ctx.restore();
 
 }
 
 function draw() {
   var ctx = document.getElementById('drawing').getContext('2d');
-  frameNum=(frameNum++)%30;
-  var translation = 100;
-  if (frameNum<16)
-  {
-    translation=200
-  }
   ctx.globalCompositeOperation = 'destination-over';
   ctx.clearRect(0, 0, 900, 700); // clear canvas
 
@@ -30,7 +34,7 @@ function draw() {
 
   ctx.save();
   ctx.lineWidth = 10;
-  ctx.translate(400, 400);
+  ctx.translate(100, 100);
   drawPic(ctx);
   ctx.restore();
 
